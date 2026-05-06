cde21ebc9cf67ad21bb140068a4439fcbb91a91c
Takuya Murakami <tmurakam@tmurakam.org>
2026-05-06 11:49:58 +0900

    Fix NoMethodError by replacing File.exists? with File.exist? (#288)
    File.exists? was removed in Ruby 3.2. Replace all occurrences with
    File.exist? to fix compatibility with Logstash 9.4.0 (JRuby 3.4).
    
    Signed-off-by: Takuya Murakami <tmurakam@tmurakam.org>

--- lib/logstash/outputs/opensearch/template_manager.rb
+++ lib/logstash/outputs/opensearch/template_manager.rb
@@ -53,7 +53,7 @@ module LogStash; module Outputs; class OpenSearch
     end
 
     def self.read_template_file(template_path)
-      raise ArgumentError, "Template file '#{template_path}' could not be found" unless ::File.exists?(template_path)
+      raise ArgumentError, "Template file '#{template_path}' could not be found" unless ::File.exist?(template_path)
       template_data = ::IO.read(template_path)
       LogStash::Json.load(template_data)
     end
